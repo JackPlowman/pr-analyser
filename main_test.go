cd4f0194c16fce39104dee6c89a601ab2fe182b6
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-16 19:32:54 +0000

    update

---
+++ main_test.go
@@ -0,0 +1,26 @@
+
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestGitHubActionSummary(t *testing.T) {
+	// Arrange
+	os.Setenv("RUNNING_IN_GITHUB_ACTION", "true")
+	os.Setenv("GITHUB_STEP_SUMMARY", "summary.txt")
+	// Act
+	GitHubActionSummary()
+	// Assert
+	content, err := os.ReadFile("summary.txt")
+	assert.NoError(t, err)
+	assert.Equal(t, "# Hello World", string(content))
+	// Clean up
+	os.Remove("summary.txt")
+	os.Unsetenv("RUNNING_IN_GITHUB_ACTION")
+	os.Unsetenv("GITHUB_STEP_SUMMARY")
+}
